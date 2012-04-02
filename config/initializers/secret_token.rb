41f871c5114f8b496f18dc6a2b7eaa24d5f69a65
Matt Topper <matt@matttopper.com>
2012-04-01 23:43:11 -0400

    new Rails app generated by Rails Apps Composer gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FiveXfifteen::Application.config.secret_token = '9c659c8e7d60d6bd74afb9bb4fa5b2a1c49994b5e0f6fddf881ce251b1ada441a3e73d170550a7f3a62660c79d4a87dce8c515536b2c1db573b91b65d2d4fc92'
